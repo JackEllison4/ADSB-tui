b8beddcdef4aec5197ba3e17d75e6261e6102fe4
Jack <jackellison772@gmail.com>
2026-05-19 23:02:37 -0300

    Added GNU Comments

--- displayEngine.py
+++ displayEngine.py
@@ -1,3 +1,10 @@
+# Copyright (C) 2026 Jack Ellison
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
 from textual.app import App, ComposeResult
 from textual.containers import Grid
 from textual.driver import Driver
